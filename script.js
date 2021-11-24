838b8d432a8b2af4b05237331a916ed3eec3952c
PrakharMishra31 <prakharmishra3197@gmail.com>
2021-11-24 21:29:30 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let time = 10;
+setTimeout(() => {
+  setTimeout(() => {
+    setTimeout(() => {
+      setTimeout(() => {
+        setTimeout(() => {
+          setTimeout(() => {
+            setTimeout(() => {
+              setTimeout(() => {
+                setTimeout(() => {
+                  setTimeout(() => {
+                    setTimeout(() => {
+                      setTimeout(() => {}, 1000);
+                      console.log(`Happy Independence Day`);
+                    }, 1000);
+                    console.log(`${time--}`);
+                  }, 1000);
+                  console.log(`${time--}`);
+                }, 1000);
+                console.log(`${time--}`);
+              }, 1000);
+              console.log(`${time--}`);
+            }, 1000);
+            console.log(`${time--}`);
+          }, 1000);
+          console.log(`${time--}`);
+        }, 1000);
+        console.log(`${time--}`);
+      }, 1000);
+      console.log(`${time--}`);
+    }, 1000);
+    console.log(`${time--}`);
+  }, 1000);
+  console.log(`${time--}`);
+}, 1000);
